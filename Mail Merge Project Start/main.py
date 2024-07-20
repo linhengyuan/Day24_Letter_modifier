91d8111aec25b27906851263bf15461fd4dcfa9e
linhengyuan <nono7794050@gmail.com>
2024-07-20 17:46:33 +0800

    letter writer machine, using read() method to fetch the starting_letter content

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -11,14 +11,12 @@
 with open("./Input/Names/invited_names.txt") as test:
     invited_list = test.readlines()
 
-with open("./Input/Letters/starting_letter.txt") as mail:
-    mail_example = mail.readlines()
-    mail_in_str = ("".join(str(x) for x in mail_example))
+with open("./Input/Letters/starting_letter.txt", mode="r") as mail:
+    mail_in_str = mail.read()
 
 # Loop through invited_list and replace the name in the letter
 for name in invited_list:
     new_name = name.strip("\n")
-    print(new_name)
+    content = mail_in_str.replace("[name]", new_name)
     with open("./Output/ReadyToSend/" + new_name + "_mail.txt", "w") as new_mail:
-        content = mail_in_str.replace("[name]", new_name)
         new_mail.write(content)
